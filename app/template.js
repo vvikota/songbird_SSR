efeacd49f1a3458c3cf9c7dc6cd155c2eace035a
Vi <rymsha.viktor@gmail.com>
2020-10-30 09:19:12 +0300

    feat: add favicon

--- app/template.js
+++ app/template.js
@@ -4,6 +4,8 @@ export default ({ body, title, store }) => (`
   <!DOCTYLE html>
   <html>
     <head>
+    <link rel="icon" href="https://vvikota-songbird.netlify.app/favicon.ico">
+    <link rel="apple-touch-icon" href="https://vvikota-songbird.netlify.app/logo192.png">
       <title>${title}</title>
     </head>
 
